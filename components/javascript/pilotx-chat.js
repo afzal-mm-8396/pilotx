69bd712119c05149bb586ac45ce67af76c9bac9b
MOHAMED SIKKANDAR AFZAL.M <mohamedafzal2102@gmail.com>
2026-03-18 19:16:53 +0530

    Fix chart data serialization and cell value rendering for nested objects

--- components/javascript/pilotx-chat.js
+++ components/javascript/pilotx-chat.js
@@ -690,7 +690,7 @@ Lyte.Component.register("pilotx-chat", {
                 }
                 console.log('CScript result came');
                 let finalResult;
-                if (result.data && typeof result.data === 'object' && !Array.isArray(result.data)) {
+                if (result.data && typeof result.data === 'object' && !Array.isArray(result.data) && result.data.data) {
                     finalResult = result.data.data;
                 } else {
                     finalResult = result.data;
