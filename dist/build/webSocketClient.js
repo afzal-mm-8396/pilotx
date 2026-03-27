8ba32d0a9536e62655e78fc168edfe649b5c1865
MOHAMED SIKKANDAR AFZAL.M <mohamedafzal2102@gmail.com>
2026-03-27 12:38:53 +0530

    wrapper view components added

--- dist/build/webSocketClient.js
+++ dist/build/webSocketClient.js
@@ -1,6 +1,6 @@
 var socket;
 function start() {	
-	socket = new WebSocket('ws://localhost:60296');
+	socket = new WebSocket('ws://localhost:54695');
 	socket.onopen = function() {
 		console.log("Connection is established");
 		socket.send("connected");
